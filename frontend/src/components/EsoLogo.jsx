2d6eacc9f6842e68449e59b92cbc4b27a68e3664
Bella Developer <bella@eso.app>
2026-06-23 16:48:42 +0300

    Fix logo path to use images/logo.png

--- frontend/src/components/EsoLogo.jsx
+++ frontend/src/components/EsoLogo.jsx
@@ -2,7 +2,7 @@
  * ESO Logo Component - Using brand image asset
  */
 
-import logoImage from '../assets/logo.png';
+import logoImage from '../assets/images/logo.png';
 
 function EsoLogo({ className = "h-10 w-auto" }) {
   return (
