f9a3855c7c8b9cc7ae3ba676696fd406b4257b61
Bella Developer <bella@eso.app>
2026-06-24 10:12:03 +0300

    Revert to clean logo without background

--- frontend/src/components/EsoLogo.jsx
+++ frontend/src/components/EsoLogo.jsx
@@ -2,25 +2,11 @@ import logoImage from '../assets/images/logo.png';
 
 function EsoLogo({ className = "h-10 w-auto" }) {
   return (
-    <div 
+    <img 
+      src={logoImage}
+      alt="ESO Logo" 
       className={className}
-      style={{
-        backgroundColor: '#000000',
-        display: 'inline-flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <img 
-        src={logoImage}
-        alt="ESO Logo" 
-        style={{
-          width: '100%',
-          height: '100%',
-          objectFit: 'contain',
-        }}
-      />
-    </div>
+    />
   );
 }
 
