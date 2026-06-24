c491e65d37b3e6a16cacdf79c1757410bbf4f2d3
Bella Developer <bella@eso.app>
2026-06-24 10:07:56 +0300

    Add black background container to logo

--- frontend/src/components/EsoLogo.jsx
+++ frontend/src/components/EsoLogo.jsx
@@ -2,11 +2,25 @@ import logoImage from '../assets/images/logo.png';
 
 function EsoLogo({ className = "h-10 w-auto" }) {
   return (
-    <img 
-      src={logoImage}
-      alt="ESO Logo" 
+    <div 
       className={className}
-    />
+      style={{
+        backgroundColor: '#000000',
+        display: 'inline-flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <img 
+        src={logoImage}
+        alt="ESO Logo" 
+        style={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'contain',
+        }}
+      />
+    </div>
   );
 }
 
