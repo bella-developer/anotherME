299513e7a71fdfcecc4b2c3489565e79fa8b04b5
bella-developer <bella-developer@users.noreply.github.com>
2026-07-13 15:25:53 +0300

    set video position to 70px

--- frontend/src/components/HorizontalHero.jsx
+++ frontend/src/components/HorizontalHero.jsx
@@ -136,7 +136,7 @@ function HorizontalHero({ onRoomChange }) {
         <motion.div
           key={room.id}
           className="absolute inset-0"
-          style={{ top: '74px' }}
+          style={{ top: '70px' }}
           initial={{ opacity: 0 }}
           animate={{ 
             opacity: currentRoom === index ? 1 : 0,
